c76114bb5c80a9af64f2ee25f2378e92beebbee9
d3dx13 <zhidkov2846@gmail.com>
2021-05-17 00:14:26 +0300

    README.MD update

--- src/cool/main.py
+++ src/cool/main.py
@@ -6,7 +6,7 @@ for image_path in os.listdir("data/original"):
     images[image_path] = cv2.imread(f"data/original/{image_path}")
 
 for name, image in images.items():
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
     image = cv2.medianBlur(image, 5)
     channels = cv2.split(image)
     for i in range(len(channels)):
